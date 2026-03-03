808e1f235bd5ae1faeabf2c5c1525410badcfaf7
SeungJae Lee <tmdwo1019@gmail.com>
2026-03-04 03:10:22 +0900

    테스트 프론트 백엔드 연결 상태 확인

--- app/models/prescriptions.py
+++ app/models/prescriptions.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from typing import TYPE_CHECKING
-
 from tortoise import fields, models
 from tortoise.fields.relational import ForeignKeyRelation
 
