781ad778f0b7debb857a73d40d39a36e3443ba51
aialice <seungmin518@gmail.com>
2026-03-04 15:02:53 +0900

    🐛 fix : mypy and code fommatting check

--- app/models/health.py
+++ app/models/health.py
@@ -39,13 +39,13 @@ class HealthChecklistLog(models.Model):
 
     id = fields.IntField(pk=True)
 
-    user: ForeignKeyRelation["User"] = fields.ForeignKeyField(
+    user: ForeignKeyRelation[User] = fields.ForeignKeyField(
         "models.User",
         on_delete=fields.CASCADE,
         related_name="health_logs",
     )
 
-    template: ForeignKeyRelation["HealthChecklistTemplate"] = fields.ForeignKeyField(
+    template: ForeignKeyRelation[HealthChecklistTemplate] = fields.ForeignKeyField(
         "models.HealthChecklistTemplate",
         on_delete=fields.CASCADE,
         related_name="logs",
