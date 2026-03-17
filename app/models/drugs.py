b1528d2e9c1fc5fa7aaa75d7fc47862a658f320f
d0ng79 <d0ng7949@gmail.com>
2026-03-17 14:06:02 +0900

    drugs스키마 수정, drugs벡터 임베딩 수정

--- app/models/drugs.py
+++ app/models/drugs.py
@@ -16,8 +16,19 @@ class Drug(models.Model):
     """
 
     id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=255)
+    name = fields.CharField(max_length=500)
     manufacturer = fields.CharField(max_length=255, null=True)
+    raw_material = fields.TextField(null=True)       # 원료성분
+    raw_material_en = fields.TextField(null=True)    # 영문성분명
+    efficacy = fields.TextField(null=True)           # 효능효과
+    dosage = fields.TextField(null=True)             # 용법용량
+    caution_1 = fields.TextField(null=True)          # 사용상의주의사항1
+    caution_2 = fields.TextField(null=True)          # 사용상의주의사항2
+    caution_3 = fields.TextField(null=True)          # 사용상의주의사항3
+    caution_4 = fields.TextField(null=True)          # 사용상의주의사항4
+    storage = fields.TextField(null=True)            # 저장방법
+    change_log = fields.TextField(null=True)         # 변경내용 (없으면 null)
+    main_ingredient = fields.TextField(null=True)    # 주성분명
 
     class Meta:
         table = "drugs"
